ff5056ba8ea0d8d2d2b864ff3347a2928b1f7af5
Seonghun Kim <sunny@seonghunui-macbook-pro.local>
2018-12-12 20:27:24 +0900

    Initial Commit

---
+++ SHConcertoMusicPlayer/SHConcertoMusicPlayer.h
@@ -0,0 +1,19 @@
+//
+//  SHConcertoMusicPlayer.h
+//  SHConcertoMusicPlayer
+//
+//  Created by Seonghun Kim on 12/12/2018.
+//  Copyright © 2018 Seonghun Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHConcertoMusicPlayer.
+FOUNDATION_EXPORT double SHConcertoMusicPlayerVersionNumber;
+
+//! Project version string for SHConcertoMusicPlayer.
+FOUNDATION_EXPORT const unsigned char SHConcertoMusicPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHConcertoMusicPlayer/PublicHeader.h>
+
+
